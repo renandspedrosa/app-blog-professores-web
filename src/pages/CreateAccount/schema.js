22db978c568d54839c3d9c3b2e5a0f7b9e0064dd
Renan Pedrosa <renandspedrosa@gmail.com>
2024-11-25 23:02:22 -0300

    schema

---
+++ src/pages/CreateAccount/schema.js
@@ -0,0 +1,14 @@
+
+import * as Yup from 'yup';
+
+const schema = Yup.object({
+    typeUser: Yup.string().required('Tipo de usuário é obrigatório'),
+    name: Yup.string().required('Nome é obrigatório'),
+    email: Yup.string().email('E-mail inválido').required('E-mail é obrigatório'),
+    password: Yup.string().required('Senha é obrigatória'),
+    confirmPassword: Yup.string()
+        .oneOf([Yup.ref('password')], 'As senhas não conferem')
+        .required('Confirmação de senha é obrigatória'),
+})
+
+export default schema;
