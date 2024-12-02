7451a46b0d731b218c8ff13ea4c72f29d67fd48c
Renan Pedrosa <renandspedrosa@gmail.com>
2024-12-02 13:30:10 -0300

    config vite

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import dotenv from 'dotenv'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': path.resolve(dirname, 'src'),
     },
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  server: {
+    port: dotenv.config().parsed.APP_PORT || 5173,
   },
 })
