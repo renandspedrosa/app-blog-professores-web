33f7354e75f8826e65077a3bb702648e5a33f6c5
Renan Pedrosa <renandspedrosa@gmail.com>
2024-12-08 08:55:47 -0300

    removendo mt form

--- src/components/Form/FormContent.jsx
+++ src/components/Form/FormContent.jsx
@@ -2,8 +2,8 @@ const Form = ({ children, ...props }) => {
     return (
         <form 
             {...props}
-            className={`bg-white rounded-lg p-12 flex flex-col md:ml-auto w-full mt-10 md:mt-0 ${props.styles}`} >
-          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">{props.title}</h2>
+            className={`bg-white rounded-lg p-12 flex flex-col md:ml-auto w-full md:mt-0 ${props.styles}`} >
+          <h2 className="text-center text-2xl/9 font-bold tracking-tight text-gray-900">{props.title}</h2>
           {children}
         </form>
     )
