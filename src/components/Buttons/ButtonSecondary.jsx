541ecbd251ba7d79b816f45aa36ef7f3d31583df
Renan Pedrosa <renandspedrosa@gmail.com>
2024-12-08 09:01:19 -0300

    mt secondary

--- src/components/Buttons/ButtonSecondary.jsx
+++ src/components/Buttons/ButtonSecondary.jsx
@@ -2,7 +2,7 @@ function ButtonSecondary(props) {
   return (
       <button 
           {...props}
-          className={`mt-6 px-4 py-2 text-white bg-blue-600 rounded-md shadow hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500 text-sm md:text-base`}
+          className={`px-4 py-2 text-white bg-blue-600 rounded-md shadow hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500 text-sm md:text-base`}
       >
           {props.children}
       </button>
