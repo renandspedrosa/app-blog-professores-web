b279ff64bf2e05daba7800a4868445529ec53760
pedrovbo <pedrovicboliveira@gmail.com>
2024-12-13 12:33:52 -0300

    ajustando card de comentário

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,4 +1,5 @@
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { Trash2 } from 'lucide-react'
 
 const Comment = ({ comment, index }) => {
   console.log(comment)
@@ -21,61 +22,28 @@ const Comment = ({ comment, index }) => {
         key={index}
       >
         <article className='p-4 text-base bg-white rounded-lg mr-1'>
-          <footer className='flex justify-between items-center mb-2'>
-            <div className='flex items-center justify-between w-full'>
-              <p className='inline-flex text-gray-400 items-center text-xs flex-4/5'>
-                {comment.user_id}
-              </p>
-              <p className='text-gray-400 flex-1/5 text-xs px-2'>
-                <time
-                  dateTime={formattedDate(comment.created_at)}
-                  title={formattedDate(comment.created_at)}
-                >
-                  {formattedDate(comment.created_at)}
-                </time>
-              </p>
-            </div>
-            <div>
-              <Menu as='div' className='relative inline-block text-left'>
-                <div>
-                  <MenuButton className='inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50'>
-                    <svg
-                      xmlns='http://www.w3.org/2000/svg'
-                      fill='none'
-                      viewBox='0 0 24 24'
-                      strokeWidth={1.5}
-                      stroke='currentColor'
-                      className='size-6'
-                    >
-                      <path
-                        strokeLinecap='round'
-                        strokeLinejoin='round'
-                        d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
-                      />
-                    </svg>
-                  </MenuButton>
-                </div>
-
-                <MenuItems
-                  transition
-                  className='absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in'
-                >
-                  <div className='py-1'>
-                    <MenuItem>
-                      <a
-                        href='#'
-                        className='block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 data-[focus]:outline-none'
-                      >
-                        Remover
-                      </a>
-                    </MenuItem>
-                  </div>
-                </MenuItems>
-              </Menu>
-            </div>
-          </footer>
+          <div className='flex items-center justify-between w-full'>
+            <p className='inline-flex text-gray-400 items-center text-xs flex-4/5'>
+              {comment.user_id}
+            </p>
+            <p className='text-gray-400 flex-1/5 text-xs px-2'>
+              <time
+                dateTime={formattedDate(comment.created_at)}
+                title={formattedDate(comment.created_at)}
+              >
+                {formattedDate(comment.created_at)}
+              </time>
+            </p>
+          </div>
           <div className='flex text-gray-600 items-center mt-4 space-x-4'>
-            <p className=''>{comment.content}</p>
+            <p>{comment.content}</p>
+          </div>
+          <div className='flex w-full justify-end'>
+            <Menu as='div'>
+              <MenuButton className='inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50'>
+                <Trash2 size={16} strokeWidth={0.75} />
+              </MenuButton>
+            </Menu>
           </div>
         </article>
       </div>
