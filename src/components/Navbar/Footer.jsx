ced8d03f5e034a1589f0207bb2c018fbb24acd70
Renan Pedrosa <renandspedrosa@gmail.com>
2024-12-17 08:55:38 -0300

    padding top footer

--- src/components/Navbar/Footer.jsx
+++ src/components/Navbar/Footer.jsx
@@ -1,12 +1,15 @@
 export const Footer = () => {
   const year = new Date().getFullYear()
   return (
+    <>
+    <div className='pt-8'></div>
     <footer className='w-full fixed bottom-0 left-0 bg-white dark:bg-gray-800'>
       <div className='max-w-screen-xl mx-auto py-3 justify-center'>
         <span className='flex justify-center text-sm text-gray-500 text-center dark:text-gray-400'>
           © {year}. Todos os Direitos Reservados.
         </span>
       </div>
-    </footer>
+      </footer>
+    </>
   )
 }
