616302373419cfea1abb8c3cc68417b7b094f9eb
Renan Pedrosa <renandspedrosa@gmail.com>
2024-12-17 08:48:56 -0300

    footer

--- src/components/Navbar/Footer.jsx
+++ src/components/Navbar/Footer.jsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   const year = new Date().getFullYear()
   return (
-    <footer className='w-full fixed bottom-0 left-0 bg-white dark:bg-gray-800 z-50'>
+    <footer className='w-full fixed bottom-0 left-0 bg-white dark:bg-gray-800'>
       <div className='max-w-screen-xl mx-auto py-3 justify-center'>
         <span className='flex justify-center text-sm text-gray-500 text-center dark:text-gray-400'>
           © {year}. Todos os Direitos Reservados.
